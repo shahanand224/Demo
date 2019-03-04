d39b231f02b9b05a7de69fc1978b27407f59118a
Anand Navinbhai Shah <anandsha@cybage.com>
2019-03-04 15:01:22 +0530

    Add Java file in Github

---
+++ src/main/java/DemoGroup/Demo1/App.java
@@ -0,0 +1,13 @@
+package DemoGroup.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
