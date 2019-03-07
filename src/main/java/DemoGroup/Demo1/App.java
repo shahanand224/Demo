5027e1396f61fa94a2108a60347e8931ff4b53b3
anandsha <anandsha@AnandSha-W8.cybage.com>
2019-03-07 15:10:18 +0530

    Commit Proper App.java

--- src/main/java/DemoGroup/Demo1/App.java
+++ src/main/java/DemoGroup/Demo1/App.java
@@ -1,21 +1,9 @@
 package DemoGroup.Demo1;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-
-    	 System.out.println( "Hello World!" );
- 		System.out.println( "Hello World!" );
- 		System.out.println( "Hello World!" );
- 		System.out.println( "Hello Arpit!" );
- 		System.out.println( "Updated" );
- 		System.out.println( "Updated second time" );
- 		System.out.println( "Changes in personal branch for test" );
 
         System.out.println( "Hello World!" );
 		System.out.println( "Hello World!" );
